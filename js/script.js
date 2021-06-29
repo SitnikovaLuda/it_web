05be0f80b44ea21bd8648ab4a7b9900a7795eda9
Luda Sitnikova <lyudochka.sitnikova@inbox.ru>
2021-06-29 19:27:58 +0400

    site v1.1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+    if(localStorage.length > 0){
+        document.getElementById('sign-out').insertAdjacentHTML('afterbegin','<div class="guest-name">'+ localStorage.key(0) +'</div>');
+        document.getElementById('sign-in').classList.add('hiden');
+        document.getElementById('sign-out').classList.remove('hiden');
+    }
+});
+
+function search(e){
+    let search_nodes = document.querySelectorAll('.one-film-name');
+    search_nodes.forEach(element => {
+        element.innerHTML = element.innerHTML.replace(/<mark>/g, '');
+        element.innerHTML = element.innerHTML.replace(/<\/mark>/g, '');
+    });
+    if((e.value.replace(/\s/g, '')).length > 0){
+        search_nodes.forEach(element => {
+            let needed = e.value.replace(/[-[\]{}()*+?.,\\^$|#\s]/gi, "\\$&");
+            if(element.innerHTML.search(new RegExp(needed, 'i')) != -1){
+                element.innerHTML = element.innerHTML.replace(new RegExp(needed, 'ig'), '<mark>' + '$&' + '</mark>');
+            }
+        });
+    }
+    if(document.getElementsByTagName('mark').length > 0){
+        document.querySelector('.big-head').style.position='fixed';
+        document.querySelector('.switch').style.marginTop = '100px';
+        document.getElementsByTagName('mark')[0].scrollIntoView({block: "center"});
+    }
+}
+
+document.getElementById('search_line').addEventListener('blur',(e)=>{
+    if(e.target.value == ""){
+        document.querySelector('.big-head').style.position='static';
+        document.querySelector('.switch').style.marginTop = '48px';
+    }
+})
+
 function sign(e){
     if(e.id == 'sign-in'){
         sign_form_popup();
@@ -5,6 +41,7 @@ function sign(e){
     else if(e.id == 'sign-out'){
         e.classList.add('hiden');
         document.getElementById('sign-in').classList.remove('hiden');
+        localStorage.removeItem(document.querySelector('.guest-name').innerHTML);
         document.querySelector('.guest-name').remove();
     }
 }
@@ -36,8 +73,19 @@ function logIn(){
         document.getElementById('sign-out').classList.remove('hiden');
         document.getElementById('sign-form').classList.add('hiden');
         document.body.style.position = 'initial';
+
+        if(radio.checked){
+            localStorage.setItem(name, pass.value);
+        }
     }
 }
+
+document.getElementById('sign-form').addEventListener('click', (e)=>{
+    if(e.target.id == 'sign-form'){
+        e.target.classList.add('hiden');
+        document.body.style.position = 'initial';
+    }
+})
 
 function films_active(e){
     if(!e.classList.contains('active-sw')){
